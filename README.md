0cad8c0c457839bec1f554e26f288ee9b29c9dc3
William Platnick <wplatnick@carnival.com>
2020-08-18 08:52:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Google Calendar to Roam
+
+This is a Python script and Alfred Workflow to export today's Google Calendar events and put them into Roam.
+
+**This is very rough and not meant to be plug and play. It is completely unsupported. I just wanted to put it out there to be a base start for others.**
+
+Demo Video:
+[![Demo Video](https://img.youtube.com/vi/x4id7c5jtTk/0.jpg)](https://youtu.be/x4id7c5jtTk)
+
+## Requirements
+
+macOS Catalina (Uses system Python 3.7
+Python 3.7
+Alfred Workflow
+
+## Setup
+
+git clone https://github.com/WilliamDurin/gcal-roam.git
+
+Go [here](https://developers.google.com/calendar/quickstart/python) and click the 'Enable the Google API' button, download the credentials.json file and stick it into the gcal-roam directory you just checked out.
+
+Run /usr/bin/pip3 -r requirements.txt
+
+Run /usr/bin/python3 quickstart.py, it should load a web page to give permissions to the app. You can ignore the insecure prompts since it's your own app.
+
+## Alfred Workflow
+
+I've also included an Alfred workflow. You'll have to edit the workflow and change it to the gcal-roam directory on your local machine.
---
+++ quickstart.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+import datetime
+import pickle
+import tzlocal
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def main():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    dirname=os.path.dirname(os.path.realpath(__file__))
+    if os.path.exists(dirname + '/token.pickle'):
+        with open(dirname + '/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                dirname + '/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(dirname + '/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    todaymin = datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time()).astimezone(tzlocal.get_localzone()).isoformat()
+    todaymax = datetime.datetime.combine(datetime.date.today(), datetime.datetime.max.time()).astimezone(tzlocal.get_localzone()).isoformat()
+ 
+    # Call the Calendar API
+    events_result = service.events().list(calendarId='primary', timeMin=todaymin, timeMax=todaymax, 
+                                        maxResults=20, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(" - ", datetime.datetime.fromisoformat(start).strftime("%I:%M %p"), " - [[", event['summary'].replace(' (copy)', ''), "]]", sep='')
+
+if __name__ == '__main__':
+    main()
